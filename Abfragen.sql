adebdd7ee18c0ad2732f5c5d580860bc12b7f176
dariohollbach <dario_hollbach@sluz.ch>
2022-06-01 14:20:46 +0200

    Create Database Skript erweitert Dario [10min]

--- Abfragen.sql
+++ Abfragen.sql
@@ -1,5 +1,4 @@
 /* Alle Übungen des Ganzkörperplans */
-
 SELECT 
 Uebung.Bez as 'Übung', 
 TrainingsplanEnthaeltUebung.Saetze AS 'Sätze', 
@@ -12,12 +11,20 @@ JOIN TrainingsplanEnthaeltUebung ON TrainingsplanEnthaeltUebung.fk_Trainingsplan
 JOIN Uebung ON Uebung.UebungID = TrainingsplanEnthaeltUebung.fk_UebungID
 JOIN Muskelgruppe ON Muskelgruppe.MuskelgruppeID = Uebung.fk_MuskelgruppeID
 WHERE Trainingsplan.Bez = 'Ganzkörper'
+GO
 
 /* Übungen, welche keinem Plan zugewiesen sind anzeigen */
 
 SELECT UebungID, Uebung.Bez as 'Übung' FROM Uebung
 LEFT JOIN TrainingsplanEnthaeltUebung ON TrainingsplanEnthaeltUebung.fk_UebungID = Uebung.UebungID
 WHERE TrainingsplanEnthaeltUebung.TrainingsplanEnthaeltUebungID IS NULL
+GO
 
-/* Eine Übung updaten */
+/* Eine Übung updaten die bereits existiert */
+/* -> Sollte fehlschlagen, da Ausfallschritt bereits exisitert*/
+UPDATE Uebung SET Bez = 'Ausfallschritt' WHERE Bez = 'Kniebeugen'
+GO
 
+/* Übersicht aller Plane*/
+SELECT Trainingsplan.Bez as [Trainingsplan], Trainingsplan.ErstellDatum as [Erstellt am], dbo.GetAllMuskelGruppenOfTrainingsplan(Trainingsplan.TrainingsplandID) AS [Muskelgruppen] FROM Trainingsplan
+GO
--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -43,3 +43,93 @@ TrainingsplanEnthaeltUebung ADD CONSTRAINT trainingsplanenthaeltuebung_fk_uebung
 ALTER TABLE
 TrainingsplanEnthaeltUebung ADD CONSTRAINT trainingsplanenthaeltuebung_fk_trainingsplanid_foreign FOREIGN KEY(fk_TrainingsplanID) REFERENCES Trainingsplan(TrainingsplandID);
 GO
+
+
+/* Trigger erstellen */
+DROP trigger if exists CheckUebungOnInsert;
+go
+
+CREATE TRIGGER CheckUebungOnInsert ON Uebung INSTEAD OF INSERT
+AS BEGIN
+	DECLARE @AnzahlDoppelteUebungen INT = (SELECT COUNT(*) as Anz
+	FROM (SELECT Uebung.Bez FROM Uebung INTERSECT SELECT inserted.bez From inserted) I);
+
+	if (@AnzahlDoppelteUebungen > 0)
+	begin
+		DECLARE @DoppelteUebungen VARCHAR(max) = (SELECT STRING_AGG (Bez, ',') AS bezeichnungen FROM (SELECT Uebung.Bez FROM Uebung INTERSECT SELECT inserted.bez From inserted) I)
+		raiserror('Folgende Übungen sind bereits vorhanden : %s', 11, 10, @DoppelteUebungen);
+		rollback transaction;
+	END
+	else
+		Insert INTO Uebung (Bez, fk_MuskelgruppeID) Select inserted.Bez, inserted.fk_MuskelgruppeID FROM inserted
+END
+GO
+
+DROP trigger if exists CheckUebungOnUpdate;
+go
+
+CREATE TRIGGER CheckUebungOnUpdate ON Uebung FOR UPDATE
+AS BEGIN
+	DECLARE @AnzahlDoppelteUebungen INT = (SELECT COUNT(*) as Anz
+	FROM (SELECT deleted.Bez FROM deleted INTERSECT SELECT inserted.bez From inserted) I);
+
+	if (@AnzahlDoppelteUebungen > 0)
+	BEGIN
+		DECLARE @DoppelteUebungen VARCHAR(max) = (SELECT STRING_AGG (Bez, ',') AS bezeichnungen FROM (SELECT deleted.Bez FROM deleted INTERSECT SELECT inserted.bez From inserted) I)
+		raiserror('Folgende Übungen sind bereits vorhanden : %s', 11, 10, @DoppelteUebungen);
+		rollback transaction;
+	END
+END
+GO
+
+
+/* Funktion erstellen */
+Drop FUNCTION IF EXISTS dbo.GetAllMuskelGruppenOfTrainingsplan;
+GO
+CREATE FUNCTION dbo.GetAllMuskelGruppenOfTrainingsplan (@trainingsPlanId INT) 
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+    declare @Result VARCHAR(max)
+    declare @Enumerator TABLE (id INT)
+
+    INSERT INTO @Enumerator
+    SELECT DISTINCT Muskelgruppe.MuskelgruppeID
+    FROM Muskelgruppe
+        JOIN Uebung ON Uebung.fk_MuskelgruppeID = Muskelgruppe.MuskelgruppeID
+        JOIN TrainingsplanEnthaeltUebung ON TrainingsplanEnthaeltUebung.fk_UebungID = Uebung.UebungID
+    WHERE fk_TrainingsplanID = @trainingsPlanId
+
+    DECLARE @id INT
+    WHILE EXISTS (SELECT 1 FROM @Enumerator)
+    BEGIN
+        SELECT TOP 1
+            @id = id
+        FROM @Enumerator
+
+        SET @Result = CONCAT(@Result, ', ', (SELECT Muskelgruppe.Bez
+        FROM Muskelgruppe
+        WHERE Muskelgruppe.MuskelgruppeID = @id))
+
+        DELETE FROM @Enumerator WHERE id = @id
+    END
+
+    RETURN right(@Result, len(@Result)-2);
+END;
+GO
+
+
+/* Prozedur Trainingsplan erstellen */
+DROP PROCEDURE if exists sp_CreateTrainingplan;
+go
+
+CREATE PROCEDURE sp_CreateTrainingplan
+    @bez varchar(50)
+AS
+BEGIN
+    INSERT INTO Trainingsplan
+        (Bez, ErstellDatum)
+    VALUES(@bez, GETDATE());
+    RETURN 1;
+END
+GO
